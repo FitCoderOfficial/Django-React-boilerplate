7455e17fd18062b2e568f22d300c7fae32e8ec4f
Shin-Sae-byeok <toqur1219@naver.com>
2018-12-28 23:55:55 +0900

    Setting and Models

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\works\pg_works\react\django-react-boilerplate\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
